5dc1eca61e35bb3d5178e647b77e5e5bbcb63a52
Jim <james.raden@gmail.com>
2023-05-03 17:00:53 -0400

    Seems to work

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var msgChan chan string
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if msgChan != nil {
+		msg := time.Now().Format(time.TimeOnly)
+		msgChan <- msg
+	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	msgChan = make(chan string)
+	defer func() {
+		close(msgChan)
+		msgChan = nil
+		fmt.Println("Client closed connection")
+	}()
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	for {
+		select {
+		case message := <-msgChan:
+			fmt.Fprintf(w, "data: %s\n\n", message)
+			flusher.Flush()
+
+		case <-r.Context().Done():
+			fmt.Printf("Client %v disconnected from SSE.\n", r.RemoteAddr)
+			return
+		}
+	}
+}
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/event", sseHandler)
+	router.HandleFunc("/time", timeHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+
+  let time = "";
+  onMount(() => {
+    const evtSrc = new EventSource("//localhost:8080/event");
+    evtSrc.onmessage = (e) => {
+      time = e.data;
+    };
+    evtSrc.onerror = (e) => {
+      console.log("EventSource failed:", e);
+    };
+  });
+
+  async function getTime() {
+    const res = await fetch("//localhost:8080/time");
+    // time = await res.text();
+    if (res.status !== 200) {
+      throw new Error(res.statusText);
+    }
+  }
+</script>
+
+<main>
+  <h1>Server Sent Events</h1>
+  <button on:click={getTime}>Get time</button>
+  <p>Time: {time}</p>
+</main>
